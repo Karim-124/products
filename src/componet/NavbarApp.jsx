1b7bcd962fb17c982c714974e3bfead1d739f569
Karim-124 <ka1465@fayoum.edu.eg>
2024-07-22 19:48:32 +0300

    call search function in navbar.

--- src/componet/NavbarApp.jsx
+++ src/componet/NavbarApp.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import logo from '../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-const NavbarApp = () => {
+const NavbarApp = ({search}) => {
+   const onSearch=(word)=>{
+    search(word);
+    console.log("s");
+   }
     return (
         <nav className="bg-red-500 p-2 flex justify-center ">
             <div className="w-3/4 relative flex justify-center items-center">
-                <input
+                <input 
+                onChange={(e)=>onSearch(e.target.value)}
                     type="text"
                     placeholder=""
-                    className="w-full p-2 rounded-full mr-10 "
+                    className="w-full p-2 pl-10 rounded-full mr-10 "
                 />
                 <FontAwesomeIcon
                     icon={faSearch}
                     className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
                 />
                 <div className="text-white font-bold text-xl"><img src={logo} className="w-20" alt="movies" /></div>
-
             </div>
-
         </nav>
     );
 };
